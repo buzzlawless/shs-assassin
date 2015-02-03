31e450df81975f981dbde42d7c23546b8f247bcc
Morgan Lawless <morganblawless@gmail.com>
2015-02-02 23:23:41 -0500

    Removed headers
    Removed email and name header with an additional pop function call in order to simplify latter code dealing with IDs.

--- functionone.py
+++ functionone.py
@@ -10,10 +10,10 @@ def importCSV(list_to_write, file_name):
 
 input_data = list()  
 importCSV(input_data, 'responses1.csv')
-input_data.pop(1)
-input_data.pop(1)
-ids = random.sample(range(len(input_data)-1), len(input_data)-1)
-print(ids)
-for i in range(1,len(input_data)):
-    input_data[i].append(ids[i-1])
+input_data.pop(0)
+input_data.pop(0)
+input_data.pop(0)
+ids = random.sample(range(len(input_data)), len(input_data))
+for i in range(0,len(input_data)):
+    input_data[i].append(ids[i])
     print(input_data[i])
