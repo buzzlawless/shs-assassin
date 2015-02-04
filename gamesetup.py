2eebe0e0b2d78948e2b4d487878c99a046a59dd9
Morgan Lawless <morganblawless@gmail.com>
2015-02-03 21:24:29 -0500

    Update gamesetup.py

--- gamesetup.py
+++ gamesetup.py
@@ -1,5 +1,8 @@
+#This should be run every 5 minutes to check if any kills have been reported in the last five minutes.  If so, update the master list and send an email and tweet.
 import csv
-import random
+import webbrowser
+import time
+import os
 import smtplib
 
 
@@ -8,16 +11,7 @@ def import_csv(list_to_write, file_name):
         reader = csv.reader(csvfile)
         for row in reader:
             list_to_write.append(row)
-    list_to_write.pop(0) #removes headers from master
-    for i in range(0,len(list_to_write)-2): #removes duplicates
-        if list_to_write[i][1] == list_to_write[i+1][1]:
-            list_to_write.pop(i)
-
-
-def generate_IDs(list_to_id):
-    ids = random.sample(range(len(list_to_id)), len(list_to_id)) #generates a list of unique random numbers from 0 to the number of participants
-    for i in range(0,len(list_to_id)):
-        list_to_id[i].append(ids[i])
+    list_to_write.pop(0) #removes headers
 
 
 def export_csv(list_to_export, file_name):
@@ -25,29 +19,38 @@ def export_csv(list_to_export, file_name):
         writer = csv.writer(csvfile)
         list_to_export.insert(0,['Timestamp', 'First and Last Name', 'Email', 'ID']) #adds headers for csv export
         writer.writerows(list_to_export)
-        list_to_export.pop(0) #removes headers from master
+        list_to_export.pop(0) #removes headers
 
 
 def get_target(index):
     if index == len(master)-1:
-        return master[0]
+        return 0
     else:
-        return master[index+1]
+        return index+1
+
+    
+def verify(index):
+    assassin_ID = kills[index][1]
+    target_ID = kills[index][2]
+    for j in range(0,len(master)):
+        if assassin_ID == master[j][3] and target_ID == master[get_target(j)][3]:
+            return j
+    return -1    
 
 
-def send_email(content, receiver):
-    gmail_user = "shsassassin15@gmail.com"
-    gmail_pwd = #password censored since GitHub is public
+def email_next_target(index):
+    gmail_user = 'shsassassin15@gmail.com'
+    gmail_pwd = ''#password censored since GitHub is public
     FROM = 'shsassassin15@gmail.com'
-    TO = [receiver] #must be a list
-    SUBJECT = "Assassin Information"
-    TEXT = content
+    TO = [master[index][2]] #must be a list
+    SUBJECT = "Your Next Assassin Target"
+    TEXT = 'This email is intended for '+master[index][1]+'.  Congratulations on eliminating '+eliminated[1]+'.  Your next target is '+master[get_target(index)][1]+'.  Good luck.'
 
     # Prepare actual message
-    message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
-    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    message = '''\From: %s\nTo: %s\nSubject: %s\n\n%s
+    ''' % (FROM, ', '.join(TO), SUBJECT, TEXT)
     try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server = smtplib.SMTP('smtp.gmail.com', 587)
         server.ehlo()
         server.starttls()
         server.login(gmail_user, gmail_pwd)
@@ -56,21 +59,13 @@ def send_email(content, receiver):
         print('successfully sent the mail')
     except:
         print('failed to send mail')
-
-
-def email_assassins():
-    for i in range(0,len(master)):
-        body = 'This email is intended for '+master[i][1]+'.  Your target is '+get_target(i)[1]+'.  Your unique ID is '+str(master[i][3])+'.'
-        body += '''  This will be your ID for the rest of the game and will NEVER change.
-Do not share your ID with anybody on the grounds of disqualification, EXCEPT when you are eliminated.
-The ONLY time you will share your ID is when you are assassinated, in which case you MUST give your ID to the person who assassinated you.
-'''
-        send_email(body, master[i][2])
-
+    
 master = list()
-import_csv(master, 'responses1.csv') #the latter parameter should be changed to match the name of the csv file
-generate_IDs(master)
-random.shuffle(master) #shuffles outer list
+kills = list()
+webbrowser.open('http://docs.google.com/spreadsheets/d/FILE_ID/export?format=csv') #replace FILE_ID with the ID of the spreadsheet
+time.sleep(5) #number of seconds to wait for file to download
+import_csv(master, 'masterlist.csv')
+import_csv(kills, 'D:\Downloads\Kill Responses - Form Responses 1.csv') #change drive letter
 '''
 master is a list of lists
 master[n] is the Nth participant's data
@@ -79,6 +74,20 @@ master[n][1] is their name
 master[n][2] is their email
 master[n][3] is their ID
 master[n+1] is their target's data (except for the last participant in the list, their target is master[0])
+
+kills is a list of lists
+kills[n] is the Nth kill reported
+kills[n][0] is the kills' timestamp
+kills[n][1] is the ID of the assassin
+kills[n][2] is the ID of the target
 '''
-export_csv(master, 'masterlist.csv')
-email_assassins()
+for i in range(0,len(kills)):
+    killer_index = verify(i)
+    if killer_index > -1:
+        eliminated = master.pop(get_target(killer_index))
+        if killer_index == len(master): #if the killer was the last person in master then everyone moves up a spot when their target was popped, hence...
+            killer_index -= 1           #... the killer's index must be reduced by 1
+        email_next_target(killer_index)
+        #email_elimination_message(get_target(killer_index))
+        #tweet(killer_index)
+os.remove('D:\Downloads\Kill Responses - Form Responses 1.csv') #change drive letter
