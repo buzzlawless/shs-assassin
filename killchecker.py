f0ca4aa06190bf2ad94309e953101af044255689
Morgan Lawless <morganblawless@gmail.com>
2015-03-13 12:43:58 -0400

    Fixed timestamp bug
    Now works with double-digit days of the month

--- killchecker.py
+++ killchecker.py
@@ -1,4 +1,5 @@
 #This should be run every 5 minutes
+#Note: timestamp only works with single digit months if you want to adapt this in the future
 import csv
 import webbrowser
 import time
@@ -26,13 +27,21 @@ def export_csv(list_to_export, file_name):
 def remove_old():
     import_csv(last_kill, 'lastkill.csv')
     timestamp1 = last_kill[0][0]
-    timestamp1 = timestamp1.replace('/',' 0',1) #replace the first / with a space followed by 0 to zero-pad day of the month for use in strptime
+    print(timestamp1)#db
+    print(timestamp1[3])#db
+    if timestamp1[3] == '/': #if single-digit day        
+        timestamp1 = timestamp1.replace('/',' 0',1) #replace the first / with a space followed by 0 to zero-pad day of the month for use in strptime
+    else:
+        timestamp1 = timestamp1.replace('/',' ',1)  #replace the first / with a space (no padding since double-digit day)
     timestamp1 = '0'+ timestamp1 #add 0 to the beginning of timestamp to zero-pad month for use in strptime
     t1 = datetime.datetime.strptime(timestamp1, '%m %d/%Y %H:%M:%S')
     i = 0
     while i < len(kills):
-        timestamp2 = kills[i][0]
-        timestamp2 = timestamp2.replace('/',' 0',1)
+        timestamp2 = kills[i][0]    
+        if timestamp2[3] == '/': #if single-digit day        
+            timestamp2 = timestamp2.replace('/',' 0',1) #replace the first / with a space followed by 0 to zero-pad day of the month for use in strptime
+        else:
+            timestamp2 = timestamp2.replace('/',' ',1)  #replace the first / with a space (no padding since double-digit day)
         timestamp2 = '0' + timestamp2
         t2 = datetime.datetime.strptime(timestamp2, '%m %d/%Y %H:%M:%S')
         latest = max((t1, t2))
@@ -108,7 +117,7 @@ kills = list()
 webbrowser.open('http://docs.google.com/spreadsheets/d/FILE_ID/export?format=csv') #replace FILE_ID with the ID of the spreadsheet
 time.sleep(5) #number of seconds to wait for file to download
 import_csv(master, 'masterlist.csv')
-import_csv(kills, 'D:\\Downloads\\Kill Survey (Responses) - Form Responses 1.csv') #change path
+import_csv(kills, 'C:\\Users\\lawlessm\\Downloads\\Kill Survey (Responses) - Form Responses 1.csv') #change path
 last_kill = list()
 remove_old()
 '''
@@ -137,4 +146,4 @@ for i in range(0,len(kills)):
         tweet(killer_index)
 export_csv(master, 'masterlist.csv')
 export_csv([kills[-1]], 'lastkill.csv')
-os.remove('D:\\Downloads\\Kill Survey (Responses) - Form Responses 1.csv') #change path
+os.remove('C:\\Users\\lawlessm\\Downloads\\Kill Survey (Responses) - Form Responses 1.csv') #change path
